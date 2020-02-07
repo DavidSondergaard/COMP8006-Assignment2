c0f832da1ad152f0d02bd0f4ffbc74756478b3b8
DavidSondergaard <dsondergaard@gmail.com>
2020-02-06 23:33:19 -0800

    Standardize LAN ip config script and start script for FW ip config

---
+++ networking/FW_ip_configuration_script.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#Lan Computer's Configuration
+LanComputerInterface=en8
+LanComputerIP=192.168.10.2
+LanComputerSubnetIP=192.168.10.0/24
+
+#Reference to Outbound Connections
+LanNameserverIp=192.168.10.1
+LanOutboundDefaultIP=192.168.10.1
+
+# TODO:
+cat > /etc/resolv.conf << EOF
+nameserver $LanNameserverIp
+options edns0
+EOF
+
+
+ip route flush dev $LanComputerInterface
+ifconfig $LanComputerInterface $LanComputerIP up
+ip route add $LanOutboundDefaultIP dev $LanComputerInterface
+ip route add default via $LanOutboundDefaultIP
+ip route add $LanComputerSubnetIP via $LanOutboundDefaultIP dev $LanComputerInterface
+route add $LanComputerSubnetIP via $LanOutboundDefaultIP $LanComputerInterface
