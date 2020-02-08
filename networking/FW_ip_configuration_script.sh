298420635172e424b7f142564e328e2d5ebeff03
groot <groot@localhost.localdomain>
2020-02-07 17:42:37 -0800

    Finish adding config info for lan

--- networking/FW_ip_configuration_script.sh
+++ networking/FW_ip_configuration_script.sh
@@ -6,57 +6,67 @@
 #                                                                  #
 ####################################################################
 
-#LAN HOST CONFIGURATION
-Lan1ComputerInterface=eno0
-Lan1SubnetIP=192.168.10.0
-Lan1Host2IP=192.168.10.2
-Lan1Host3IP=192.168.10.3
-Lan1Host4IP=192.168.10.4
 
-#ADMIN HOST CONFIGURATION
-Lan2ComputerInterface=eno2
-Lan1SubnetIP=192.168.20.0
-Lan1Host2IP=192.168.20.2
+IPExactMatch=255.255.255.255
+#LAN HOST CONFIGURATION
+Lan1Host2InterfaceName=enp60s0
+Lan1Host2Name=apples
+Lan1Host2InterfaceIP=192.168.10.2
+Lan1DefaultGateway=192.168.10.1
+Lan1SubnetIP=192.168.10.0
+Lan1SubnetMask=255.255.255.0
+
+Lan1Host3InterfaceIP=UNASSIGNED
 
 #FIREWALL HOST CONFIGURATION
-LanNameserverIp=192.168.10.1
-DefaultLan1OutboundIP=192.168.10.1
-DefaultLan2OutboundIP=192.168.20.1
-DefaultWanOutboundIP=192.168.1.254
+FWLan1InterfaceName=enp0s20f0u3u3
+FWLan1SubnetName=orchards
+FWLan1InterfaceIP=192.168.10.1
+FWLan1NameserverIp=192.168.10.1
+FWLan1SubnetIP=$Lan1SubnetIP
+FWLan1SubnetMask=$Lan1SubnetMask
+
+FWWanInterfaceName=enp0s20f0u2
+FWDefaultWanOutboundName=galaxies
+FWWanInterfaceIP=192.168.1.130
+FWWanDefaultGateway=192.168.1.254
+FWWanSubnetIP=192.168.1.0
+FWWanSubnetMask=255.255.255.0
+
+if [  "$1" = "firewall"  ]
+    then
+            echo "setting firewall config because $1 was passed"
+        #CLEAR rules
+            ip route flush dev $FWLan1InterfaceName
+        #SET outbound interface
+            ifconfig $FWLan1InterfaceName $FWLan1InterfaceIP up
+        #ENABLE IP forwarding between interfaces
+            echo "1" | sudo tee /proc/sys/net/ipv4/ip_forward
+        #ADD FIREWALL DEFAULT UNKNOWN IP GATEWAY TO WAN INTERFACE
+            route add -net $FWWanSubnetIP netmask $FWWanSubnetMask gw $FWWanInterfaceIP
+        #FORCE ALL SUBNET TRAFFIC TO USE FIREWALL INTERFACE TO ACCESS LAN1 ORCHARD SUBNET
+            route add -net $FWLan1SubnetIP netmask $FWLan1SubnetMask gw $FWLan1InterfaceIP
+    elif [  "$1" = "lan1"  ]
+    then
+        #CLEAR rules
+            echo "setting lan1 config because $1 was passed"
+            ip route flush dev $Lan1Host2InterfaceName
+        #SET outbound interface
+            ifconfig $Lan1Host2InterfaceName $Lan1Host2IP up
+        #ENABLE IP forwarding between interfaces
+            ip route add $Lan1DefaultGateway dev $Lan1Host2InterfaceName
+        #SEND TRAFFIC TO FIREWALL IF NO VALID MATCH
+            ip route add default via $Lan1DefaultGateway
+        #SEND TRAFFIC TO FIREWALL FIRST IF REQUEST IS FOR A COMPUTER IN THIS SUBNET
+            ip route add $Lan1SubnetIP netmask $FWLan1SubnetMask via $Lan1DefaultGateway dev $Lan1Host2InterfaceName
+            route add $Lan1SubnetIP netmask $FWLan1SubnetMask via $Lan1DefaultGateway $Lan1Host2InterfaceName
+    else
+        #error
+        echo "$1 is not a valid match.  Please try again."
+fi
 
 
 
-#
-# TODO:
-#cat > /etc/resolv.conf << EOF
-#nameserver $LanNameserverIp
-#options edns0
-#EOF
-
-#CLEAR rules
-ip route flush dev $LanComputerInterface
-
-#SET outbound interface
-ifconfig $LanComputerInterface $LanComputerIP up
-
-#ENABLE IP forwarding between interfaces
-echo "1" | sudo tee /proc/sys/net/ipv4/ip_forward
-
-
-
-#ADD FIREWALL DEFAULT UNKNOWN IP GATEWAY
-route add -net  192.168.0.0 netmask 255.255.255.0 gw WanOutboundDefaultIP
-#ADD FIREWALL DEFAULT ROUTE TO ORCHARD SUBNET
-route add -net 192.168.10.0/24 gw $LanOutboundDefaultIP
-#ADD FIREWALL DEFAULT ROUTE TO WATERWAYS SUBNET
-route add -net 192.168.20.0/24 gw $AdminOutboundDefaultIP
-
-
-#ADD FIREWALL DEFAULT ROUTE TO WATERWAYS SUBNET
-ip route add $LanComputerSubnetIP via $LanOutboundDefaultIP dev $LanComputerInterface
-
-#
-route add $LanComputerSubnetIP via $LanOutboundDefaultIP $LanComputerInterface
 
 
 ###################################################################
