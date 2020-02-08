6a2a931ae2765989a115653ee8730091a159047e
DavidSondergaard <dsondergaard@gmail.com>
2020-02-08 00:52:05 -0800

    WAN CONFIGURATION COMPLETE

--- networking/FW_ip_configuration_script.sh
+++ networking/FW_ip_configuration_script.sh
@@ -33,6 +33,14 @@ FWWanDefaultGateway=192.168.1.254
 FWWanSubnetIP=192.168.1.0
 FWWanSubnetMask=255.255.255.0
 
+#LAN HOST CONFIGURATION
+Wan1Host2InterfaceName=eth0
+Wan1Host2Name=attackers
+Wan1Host2InterfaceIP=192.168.1.135
+Wan1DefaultGateway=192.168.1.254
+Wan1SubnetIP=192.168.1.0
+Wan1SubnetMask=255.255.255.0
+
 if [  "$1" = "firewall"  ]
     then
             echo "setting firewall config because $1 was passed"
@@ -61,84 +69,24 @@ if [  "$1" = "firewall"  ]
             echo "setting C"
             route add default gw $Lan1DefaultGateway
         #SEND TRAFFIC TO FIREWALL FIRST IF REQUEST IS FOR A COMPUTER IN THIS SUBNET
+    elif [  "$1" = "wanClient"  ]
+    then
+        #CLEAR rules
+            echo "setting wanClient config because $1 was passed"
+            #ip route flush dev $Lan1Host2InterfaceName
+        #SET outbound interface
+            
+            ifconfig $Wan1Host2InterfaceName $Wan1Host2InterfaceIP up
+        #ROUTING TRAFFIC TO FIREWALL THROUGH SUBNET TO CLIENT LAN
+            echo "setting A"
+            route add default gw $Wan1DefaultGateway
+            echo "setting B"
+            route add default gw $Wan1DefaultGateway dev $Wan1Host2InterfaceName
+            echo "setting C"
+            route add default gw $Wan1DefaultGateway via $FWWanInterfaceIP dev $Wan1Host2InterfaceName
+            echo "setting D"
+            route add default gw $Wan1DefaultGateway gw $FWWanInterfaceIP
     else
         #error
         echo "$1 is not a valid match.  Please try again."
 fi
-
-
-
-
-
-###################################################################
-#                                                                 #
-#                    SETUP HOSTS AND ALIASES                      #
-#                                                                 #
-###################################################################
-#
-# IP ---------- FQDN ----------------------------------- ALIASES
-#
-#WAN AND LOCAL INTERFACES
-#127.0.0.1       localhost                                planet3-landing
-#192.168.1.87    planet3                                  planet3-if0
-#192.168.1.254   galaxies                                 galaxies-wan            
-#
-#FIREWALL INTERNAL INTERFACE FOR SUBNET 10
-#172.16.10.1     orchards.bcit8006-pts20-assign2.com      orchards planet3-if1
-#LAN HOST 2
-#72.16.10.2     apples.bcit8006-pts20-assign2.com         apples
-#LAN HOST 3
-#72.16.10.3     oranges.bcit8006-pts20-assign2.com        oranges
-#LAN HOST 4
-#72.16.10.4     pears.bcit8006-pts20-assign2.com          pears
-
-#ADMIN INTERNAL HARDWIRED INTERFACE
-#FIREWALL INTERFACE FOR SUBNET 20
-#172.16.20.1     waterways.bcit8006-pts20-assign2.com     waterways planet3-if2 
-#ADMIN HOST INTERFACE
-#172.16.20.2     sharkbait.bcit8006-pts20-assign2.com     sharkbait
-
-###
-# THIS PRINTS THE NAMESERVERS OF THE FIREWALL COMPUTER
-#
-# nmcli -o | grep servers | awk '{print $2}'
-
-# TO ADD ANOTHER DNS
-
-# nmcli -t con | grep enp0s20f0u2 | awk -F ":" '{print $1}'
-
-##### val1=`(jmcli -t con show | grep enp | awk -F ":" '{ print $1}')`; nmcli con modify $val1 +ipv4.dns "1.2.3.6"
-
-
-# nmcli con modify "Wired connection 2" +ipv4.dns "192.168.1.254"
-
-# nmcli con down "Wired connection 2"
-# nmcli con up "Wired connection 2"
-###
-
-
-
-
-
-# sudo iptables -t nat -A POSTROUTING -o enp0s20f0u2 -j MASQUERADE
-# sudo iptables -A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
-# sudo iptables -A FORWARD -i enp0s20f0u3u3 -o enp0s20f0u2 -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT
-# sudo iptables -A FORWARD -i enp0s20f0u2 -o enp0s20f0u3u3 -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT
-
-
-
-
-
-
-###OUTSIDE OF NETOWRK!!!
-
-# TO ACCESS INSIDE OF NETOWRK FROM OUTSIDE, YOIU NEED TO SETUP FORWARDING FROM OUTSIDE TO INSIDE. 
-# THEN, YOU ALSO NEED TO SETUP THE ROUTING TABLES ON THE WAN COMPUTER TO RECOGNIZE THAT THE
-# SUBNET IS ACCESSED FROM THE FW WAN SIDE INTERFACE
-
-# SO ON THE COMPUTER THAT IS ON THE WAN, ENTER:  
-
-#route add -net 192.168.10.0/24
-#route add -net 192.168.10.0/24 dev eth0
-#route add -net 192.168.10.0/24 via 192.168.1.130 dev eth0
-#route add -net 192.168.10.0/24 gw 192.168.1.130
